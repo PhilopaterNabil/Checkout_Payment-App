33e3a22a7f47975772042426fdfb7be44b8efd5e
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-15 21:06:41 +0300

    Refactor PaymentMethodItem to include onTap callback and convert PaymentMethods to StatefulWidget for dynamic selection

--- lib/features/checkout/presentation/views/widgets/payment_method_item.dart
+++ lib/features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -2,33 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class PaymentMethodItem extends StatelessWidget {
-  const PaymentMethodItem({super.key, required this.imagePath, this.isSelected = false});
+  const PaymentMethodItem(
+      {super.key, required this.imagePath, this.isSelected = false, required this.onTap});
 
   final String imagePath;
   final bool isSelected;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 600),
-      height: 62,
-      width: 103,
-      margin: EdgeInsets.only(left: 20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          side: isSelected
-              ? BorderSide(color: Color(0xFF34A853), width: 1.5)
-              : BorderSide(color: Colors.black.withOpacity(0.5), width: 1),
-          borderRadius: BorderRadius.circular(15),
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        height: 62,
+        width: 103,
+        margin: EdgeInsets.only(left: 20, top: 2),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            side: isSelected
+                ? BorderSide(color: Color(0xFF34A853), width: 1.5)
+                : BorderSide(color: Colors.black.withOpacity(0.5), width: 1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          shadows: isSelected
+              ? [
+                  BoxShadow(color: Color(0xFF34A853), blurRadius: 4),
+                ]
+              : null,
         ),
-        shadows: isSelected
-            ? [
-                BoxShadow(color: Color(0xFF34A853), blurRadius: 4),
-              ]
-            : null,
+        child: Center(child: SvgPicture.asset(imagePath)),
       ),
-      child: Center(child: SvgPicture.asset(imagePath)),
     );
   }
 }
--- lib/features/checkout/presentation/views/widgets/payment_methods.dart
+++ lib/features/checkout/presentation/views/widgets/payment_methods.dart
@@ -2,16 +2,40 @@ import 'package:checkout_payment_app/core/utils/app_image_assets.dart';
 import 'package:checkout_payment_app/features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/material.dart';
 
-class PaymentMethods extends StatelessWidget {
+class PaymentMethods extends StatefulWidget {
   const PaymentMethods({super.key});
+
+  @override
+  State<PaymentMethods> createState() => _PaymentMethodsState();
+}
+
+class _PaymentMethodsState extends State<PaymentMethods> {
+  int activeIndex = 0;
+  void setActiveIndex(int index) {
+    setState(() {
+      activeIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        PaymentMethodItem(imagePath: AppImageAssets.imagesCard, isSelected: true),
-        PaymentMethodItem(imagePath: AppImageAssets.imagesPaypal),
-        PaymentMethodItem(imagePath: AppImageAssets.imagesMasterCard),
+        PaymentMethodItem(
+          onTap: () => setActiveIndex(0),
+          imagePath: AppImageAssets.imagesCard,
+          isSelected: activeIndex == 0,
+        ),
+        PaymentMethodItem(
+          onTap: () => setActiveIndex(1),
+          imagePath: AppImageAssets.imagesPaypal,
+          isSelected: activeIndex == 1,
+        ),
+        PaymentMethodItem(
+          onTap: () => setActiveIndex(2),
+          imagePath: AppImageAssets.imagesMasterCard,
+          isSelected: activeIndex == 2,
+        ),
       ],
     );
   }
