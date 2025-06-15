01826085b262d93dd05cf32d7d842fc0b58b647b
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-15 16:51:32 +0300

    Add SVG assets for MasterCard and PayPal, and update asset management
    - Added master_card.svg and paypal.svg to the assets/images directory.
    - Created AppImageAssets class to manage image asset paths.
    - Introduced app_text_styles.dart for consistent text styling across the app.
    - Updated pubspec.yaml to include flutter_svg dependency and organized asset paths.
    - Updated pubspec.lock to reflect new dependencies and versions.

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle font18BlackRegular = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle font20BlackRegular = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Inter',
+    color: Colors.black.withOpacity(0.8),
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle font25BlackMedium = TextStyle(
+    fontSize: 25,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle font18BlackSemiBold = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle font24BlackSemiBold = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+}
